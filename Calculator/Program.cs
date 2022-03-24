f5d2ea7aed9e100ec44f67d2aff5cb2b002cef72
rogerch93 <rogerinhochaves@hotmail.com>
2022-03-24 08:27:11 -0300

    first commit

---
+++ Calculator/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Escolha qual tipo de calculo deseja realizar:");
+            Console.WriteLine("1 - Soma");
+            Console.WriteLine("2 - Subtração");
+            Console.WriteLine("3 - Multiplicação");
+            Console.WriteLine("4 - Divisão");
+            Console.WriteLine("5 - Sair");
+
+            Console.WriteLine("_____________________________________________");
+            Console.WriteLine("Digite a opção desejada:");
+
+            short resultado = short.Parse(Console.ReadLine());
+
+            switch (resultado)
+            {
+                case 1:
+                    Soma();
+                break;
+                case 2:
+                    Subtracao();
+                break;
+                case 3:
+                    Multiplicacao();
+                break;
+                case 4:
+                    Divisao();
+                break;
+                case 5:
+                    System.Environment.Exit(0);
+                break;
+            }
+        }
+
+        static void Soma()
+        {
+            Console.WriteLine("Digite o primeiro numero:");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo numero:");
+            int num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"O resultado da soma é: {num1 + num2}");
+            Console.ReadKey();
+            Menu();
+        }
+
+        static void Subtracao()
+        {
+            Console.WriteLine("Digite o primeiro numero:");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo numero:");
+            int num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"O resultado da subtração é: {num1 - num2}");
+            Console.ReadKey();
+            Menu();
+        }
+
+        static void Multiplicacao()
+        {
+            Console.WriteLine("Digite o primeiro numero:");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo numero:");
+            int num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"O resultado da multiplicação é: {num1 * num2}" );
+            Console.ReadKey();
+            Menu();
+        }
+
+        static void Divisao()
+        {
+            Console.WriteLine("Digite o primeiro numero:");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo numero:");
+            int num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"O resultado da divisão é: {num1 / num2}");
+            Console.ReadKey();
+            Menu();
+        }
+        
+        
+    }
+}
---
+++ Calculator/README.md
@@ -0,0 +1,16 @@
+# Calculadora
+
+## Crie este simples projeto de calculadora para comparar a estrutura do código .NET 3.1 versão mais antiga com a .NET 6.0 versão mais nova.
+
+> ## Calculadora criada com C# .net6
+> ### Foi Utilizado
+> #### - Funções 
+> #### - Switch Case 
+> #### - String Interpolation
+> #### - Convert 
+
+# Como utilizar 
+#### Primeiramente o usuário irá escolher qual tipo de cálculo ira utilizar selecionando as opções existentes na tela
+#### Devera inserir 1 numero por vez para que a soma seja executada 
+#### Após o término do cálculo ele retornará ao menu inicial novamente para um novo cálculo
+#### Caso ele queira sair do sistema deverá usar a opção de numero 4
